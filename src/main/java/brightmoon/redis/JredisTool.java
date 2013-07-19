e7edc6eab3ed94f051f8a607888a3bef2830a453
renjie120 <lsq870420>
2013-07-20 00:46:27 +0800

    update jedis_tool

---
+++ src/main/java/brightmoon/redis/JredisTool.java
@@ -0,0 +1,84 @@
+package brightmoon.redis;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * 基础的redis操作封装. 只提供关于只读方法的封装，对于写操作可能涉及到事务等操作,示例代码：DictionaryRedis.java
+ * 
+ * @author lisq
+ * 
+ */
+public class JredisTool implements IRedisTool {
+	private RedisTemplate template;
+	public RedisTemplate getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(RedisTemplate template) {
+		this.template = template;
+	}
+
+	@Override
+	public List<byte[]> getList(byte[] k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getKey(byte[] k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getLen(byte[] k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsKey(byte[] key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object getVal(byte[] key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getJsonData(byte[] key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<byte[]> allKeys(byte[] key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long dbSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getConfig(String str) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePreKeys(String key) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+}
